f6da237b19b5b10fc3c78b3a238fe459380b3159
Renu <saraswathy.renuga@tcs.com>
2014-12-25 14:26:20 +0530

    css changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,11 +6,17 @@ class Post < ActiveRecord::Base
   	validate :description,:length=>{:minimum=>10}
   	attr_accessor :category_slug  	
   	belongs_to :category
-	before_save :set_category
+	before_save :set_category,:validate_title_and_description
 
   	def set_category
 		self.category=Category.find_by_slug(self.category_slug) if self.category_slug
 	end
+
+  def validate_title_and_description    
+    self.errors.add(:title,"is blank") if self.title==""
+    self.errors.add(:description,"is blank") if self.description==""
+  end
+
 
   def short_description
     if self.description.length>300
