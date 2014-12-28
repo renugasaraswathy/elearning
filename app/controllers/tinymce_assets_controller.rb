11e19f997a3247c94b4159b070936a5f7921349f
Renu <saraswathy.renuga@tcs.com>
2014-12-28 16:11:30 +0530

    debugging image upload in production

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -2,17 +2,12 @@ class TinymceAssetsController < ApplicationController
   def create
 	geometry = Paperclip::Geometry.from_file params[:file]	
 
-  puts "\n\n\n\n image params"  
-  puts image_params
-  puts image_params.methods
-  puts image_params.inspect
-
-    image=Image.new(:picture=>image_params[:file])
-    #file=File.open(image_params)
-    #image.picture=file
-    #file.close
-    image.save
-    
+  
+    image=Image.new(:picture=>image_params[:file])    
+    if image.save
+    else
+      image.errors.full_messages
+    end
     render json: {
       image: {
         url: image.picture.url
